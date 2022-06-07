359e0ebba4aa5635e79aa286b9e40da6de4e5320
Mohit Bajaj <mohitbajaj@Mohits-MacBook-Pro.local>
2022-06-08 01:57:38 +0530

    Updates

---
+++ precache-manifest.ffae7c149020123d54c7c29ad12c7964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c81de0a3c0afa783fbdc239be7b8717",
+    "url": "/githubfinder/index.html"
+  },
+  {
+    "revision": "8ff79896befc04abb805",
+    "url": "/githubfinder/static/css/main.a689a578.chunk.css"
+  },
+  {
+    "revision": "2993e287b516027314e1",
+    "url": "/githubfinder/static/js/2.047add32.chunk.js"
+  },
+  {
+    "revision": "8ff79896befc04abb805",
+    "url": "/githubfinder/static/js/main.3c726249.chunk.js"
+  },
+  {
+    "revision": "d5fcb47b8eef770284ba",
+    "url": "/githubfinder/static/js/runtime-main.a6e2a531.js"
+  },
+  {
+    "revision": "bbf639060ebc175fea350ada3d4a5f7b",
+    "url": "/githubfinder/static/media/spinner-icon-gif-10.bbf63906.jpg"
+  }
+]);
